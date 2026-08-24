c6f9f644ca4df9f2a2d8ad32f59400c47ff949ab
Abidemi Awojinrin <awojinrin3@gmail.com>
2026-08-24 18:11:51 -0400

    Add First Java program

---
+++ src/First.java
@@ -0,0 +1,10 @@
+// Name: Abidemi Awojinrin
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Computer Systems Analyst");
+        System.out.println("Why: I want to help organizations improve their computer systems and operate more efficiently.");
+        System.out.println("Median salary: $103,790 per year");
+    }
+}

53b6b7af6d2bb399f288ad48eb2c3e717b79a852
Abidemi Awojinrin <awojinrin3@gmail.com>
2026-09-04 19:30:21 -0400

    Add comments to Tests program

--- src/Tests.java
+++ src/Tests.java
@@ -1,14 +1,24 @@
+// Name: Abidemi Awojinrin
+// This program calculates and displays the average of three test scores.
+
 public class Tests {
     public static void main(String[] args) {
+
+        // Declare and initialize the three test scores.
         double testScore1 = 88.2;
         double testScore2 = 78.9;
         double testScore3 = 97.6;
 
+
+        // Calculate the average of the three test scores
         double average = (testScore1 + testScore2 + testScore3) / 3.0;
 
+        // Display each test score.
         System.out.println("Test score 1:   " + testScore1);
         System.out.println("Test score 2:  " + testScore2);
         System.out.println("Test score 3:  " + testScore3);
+
+        //Display the average rounded to two decimal places.
         System.out.printf("The average of 3 test scores is:  %.2f%n", average);
     }
 }
